768df365d5f20a1fce0e46f863609dc5c087d3e6
Liam Stevens <liamstevens111@gmail.com>
2018-01-04 11:15:16 +0700

    added the Tweets app and related views, urls and models

---
+++ src/tweets/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+#Create
+
+#Retreive
+def tweet_detail_view(request, id = 1):
+	return render(request, "tweets/detail_view.html", {})
+
+def tweet_list_view(request):
+	return render(request, "tweets/list_view.html", {})
+
+#Update
+
+#Delete
